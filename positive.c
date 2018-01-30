84266feac419a5d51bc2d55def928b450ee73fd2
parivarthini <35654030+parivarthini@users.noreply.github.com>
2018-01-30 15:57:03 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int c;
+clrscr();
+scanf("%d",&c);
+if(c==0)
+{
+printf("c is zero");
+}
+else if(c>0)
+{
+printf("c is positive");
+}
+else
+{
+printf("c is negative");
+}
+getch();
+}
+
+
